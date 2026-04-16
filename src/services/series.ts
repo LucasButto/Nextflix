567836497b9dbf669bed7af5aa0b7f7ff76e6648
Lucas Butto <93658061+LucasButto@users.noreply.github.com>
2026-04-16 15:06:03 -0300

    Expand page range in getTop100Series function

--- src/services/series.ts
+++ src/services/series.ts
@@ -50,7 +50,7 @@ const TOP100_SERIES_MIN_VOTES = 1750;
 
 export async function getTop100Series() {
   const pages = await Promise.all(
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((page) =>
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30].map((page) =>
       tmdbFetch<TmdbListResponse<Series>>("/tv/top_rated", { page })
         .then((d) => filterLatinScript(d.results))
         .catch(() => [] as Series[]),
